c1bfa8a484966672bbc91a4245161bb8bc75b59b
Jason Chan <jasonnchann24@gmail.com>
2021-03-08 19:46:42 +0700

    login done

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,94 +1,51 @@
 @extends('layouts.app')
 
+@section('additional-head')
+<link rel="stylesheet" href="{{asset('css/signin.css')}}">
+@endsection
+
 @section('content')
-<div class="min-vh-100">
-    <div class="jumbotron vertical-center">
-        <div class="container my-auto">
-            <div class="row justify-content-center">
-                <div class="col-md-8">
-                    <div class="card">
-                        <div class="card-header">{{ __('Login') }}</div>
+<div class="signin-body">
+  <main class="form-signin">
+    <form method="POST" action="{{ route('login') }}">
+      @csrf
 
-                        <div class="card-body">
-                            <form method="POST" action="{{ route('login') }}">
-                                @csrf
+      <img class="mb-4" src="{{asset('logo.png')}}" alt="aryanoble logo" width="100">
+      <h1 class="h5 mb-3 fw-normal mt-2">Recruitment Management System</h1>
+      <div class="form-outline">
+        <input id="email" type="email"
+          class="form-control form-control-lg bg-white @error('email') is-invalid @enderror" name="email"
+          placeholder="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-                                <div class="form-group row">
-                                    <label for="email"
-                                        class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+        <label for="email" class="form-label">Email address</label>
+        @error('email')
+        <span class="invalid-feedback" role="alert">
+          <strong>{{ $message }}</strong>
+        </span>
+        @enderror
+      </div>
 
-                                    <div class="col-md-6">
-                                        <input id="email" type="email"
-                                            class="form-control @error('email') is-invalid @enderror"
-                                            name="email"
-                                            value="{{ old('email') }}" required
-                                            autocomplete="email" autofocus>
+      <div class="form-outline mt-3">
+        <input id="password" type="password"
+          class="form-control form-control-lg bg-white @error('password') is-invalid @enderror" name="password"
+          placeholder="password" required autocomplete="current-password">
+        <label for="password" class="form-label">Password</label>
 
-                                        @error('email')
-                                        <span class="invalid-feedback"
-                                            role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                        @enderror
-                                    </div>
-                                </div>
+        @error('password')
+        <span class="invalid-feedback" role="alert">
+          <strong>{{ $message }}</strong>
+        </span>
+        @enderror
+      </div>
 
-                                <div class="form-group row">
-                                    <label for="password"
-                                        class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                                    <div class="col-md-6">
-                                        <input id="password" type="password"
-                                            class="form-control @error('password') is-invalid @enderror"
-                                            name="password" required
-                                            autocomplete="current-password">
-
-                                        @error('password')
-                                        <span class="invalid-feedback"
-                                            role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                        @enderror
-                                    </div>
-                                </div>
-
-                                <div class="form-group row">
-                                    <div class="col-md-6 offset-md-4">
-                                        <div class="form-check">
-                                            <input class="form-check-input"
-                                                type="checkbox" name="remember"
-                                                id="remember"
-                                                {{ old('remember') ? 'checked' : '' }}>
-
-                                            <label class="form-check-label"
-                                                for="remember">
-                                                {{ __('Remember Me') }}
-                                            </label>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="form-group row mb-0">
-                                    <div class="col-md-8 offset-md-4">
-                                        <button type="submit"
-                                            class="btn btn-primary">
-                                            {{ __('Login') }}
-                                        </button>
-
-                                        @if (Route::has('password.request'))
-                                        <a class="btn btn-link"
-                                            href="{{ route('password.request') }}">
-                                            {{ __('Forgot Your Password?') }}
-                                        </a>
-                                        @endif
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+      <button class="w-100 btn btn-lg btn-primary mt-4" type="submit">Sign in</button>
+      @if (Route::has('password.request'))
+      <a class="btn btn-link mt-2" href="{{ route('password.request') }}">
+        {{ __('Forgot Your Password?') }}
+      </a>
+      @endif
+      <p class="mt-5 mb-3 text-muted">&copy; 2021 Arya Noble</p>
+    </form>
+  </main>
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,33 +1,34 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- CSRF Token -->
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>Recruitment Management System | Arya Noble</title>
+    <title>Recruitment Management System | Arya Noble</title>
 
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
-        <!-- Fonts -->
-        <link rel="dns-prefetch" href="//fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css?family=Nunito"
-            rel="stylesheet">
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet">
 
-        <!-- Styles -->
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    </head>
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
 
-    <body>
-        <div id="app">
-            <main>
-                @yield('content')
-            </main>
-        </div>
-    </body>
+    @yield('additional-head')
+</head>
+
+<body style="height: 100vh">
+    <div id="app">
+        @yield('content')
+    </div>
+</body>
 
 </html>
