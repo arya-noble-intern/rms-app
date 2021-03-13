b4c47d2a333bd10e0731bdc892ae126abbaca81a
JasonChan <jasonnchann24@gmail.com>
2021-03-13 17:51:23 +0700

    undo debounce mixin

--- resources/js/mixins/Global.js
+++ resources/js/mixins/Global.js
@@ -4,18 +4,6 @@ export default {
             const delayPromise = ms =>
                 new Promise(resolve => setTimeout(resolve, ms));
             await delayPromise(x);
-        },
-
-        $debounce(fn, delay) {
-            var timeoutID = null;
-            return function() {
-                clearTimeout(timeoutID);
-                var args = arguments;
-                var that = this;
-                timeoutID = setTimeout(function() {
-                    fn.apply(that, args);
-                }, delay);
-            };
         }
     }
 };
