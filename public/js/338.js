cfc20b1d4947c3a8f89639b7a76a21f2791fa16d
JasonChan <jasonnchann24@gmail.com>
2021-03-15 10:24:38 +0700

    wip cards

---
+++ public/js/338.js
@@ -0,0 +1,2 @@
+(self.webpackChunk=self.webpackChunk||[]).push([[338],{2338:(e,n,t)=>{"use strict";t.r(n),t.d(n,{default:()=>c});const a={name:"CandidateCardShow"};const c=(0,t(1900).Z)(a,(function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)}),[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h1",[e._v("candidatecardshow")])])}],!1,null,null,null).exports}}]);
+//# sourceMappingURL=338.js.map
